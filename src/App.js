a7669c80db889df002f2918fc86da54498a2d019
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-05 13:01:07 +0000

    finish nav componet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.css';
 import Header from "./componets/Header";
+import Nav from "./componets/Nav";
 
 function App() {
   return (
     <div className="app">
         <Header/>
+        <Nav/>
 
-        {/*navbar*/}
         {/*results*/}
     </div>
   );
